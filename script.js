d0d5d097520bee5ceaeec2a3fdf7a112177b975e
Dina-dev <winter2334@gmx.fr>
2025-04-16 14:14:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const taskContainer = document.querySelector('.taskContainer');
+const field = document.querySelector ('.field');
+const btn = document.querySelector('.btn');
+
+
+btn.addEventListener('mouseover', () => {
+  btn.classList.add('mouseOn');
+  btn.classList.remove('mouseOff');
+});
+
+btn.addEventListener('mouseout', () => {
+  btn.classList.remove('mouseOn');
+  btn.classList.add('mouseOff');
+});
+
+btn.addEventListener('click', addTask);
+
+field.addEventListener('keypress', function(e) {
+  if (e.keyCode === 13) {
+    addTask();
+  }
+});
+
+function addTask() {
+  if (field.value.trim() === '') {
+    return; // Empêcher l'ajout de tâches vides
+  }
+  
+  const item = document.createElement('li');
+  item.innerText = field.value;
+  item.classList.add('taskAdded');
+  taskContainer.appendChild(item);
+  field.value = '';
+  
+  item.addEventListener('click', () => {
+    item.classList.add('taskCompleted');
+  });
+
+  item.addEventListener('dblclick', () => {
+    taskContainer.removeChild(item);
+  });
+}
+
+
+
+
+
+/*btn.addEventListener ('click', ()=> {
+  
+    const item = document.createElement ('li');
+    item.innerText = field.value;
+    item.classList.add('taskAdded');
+    taskContainer.appendChild(item);
+    field.value ='';
+    
+    item.addEventListener ('click', ()=>{
+      item.classList.add('taskCompleted');
+    })
+    item.addEventListener ('dblclick', ()=>{
+       taskContainer.removeChild (item);
+  })   
+   
+
+})*/
+
+
